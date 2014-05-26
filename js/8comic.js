9910f6a90745fbb4233be91a840b9dca850446e1
jhow <jhowliu@gmail.com>
2014-05-26 15:43:38 +0800

    LoadNext need to bd fixed.

--- js/8comic.js
+++ js/8comic.js
@@ -1,8 +1,11 @@
 var firstPage = location.origin + location.pathname + location.search;
+var nextVolURL, ch, host;
 var listener = new Object();
-var calPictureURL = function (cs, ch, host, callback) {
-		var pagenum, patten, ans, c;
-		host = host + '/' + ch;
+var loading = false;
+
+var calPictureURL = function (cs, callback) {
+		var pagenum, patten, ans, c, curHost;
+		curHost = host + '/' + ch;
 		c = "";
 
 		for (var index = 0; index <= cs.length/50; index++) {
@@ -21,7 +24,7 @@ var calPictureURL = function (cs, ch, host, callback) {
 		for(var p = 1; p <= pagenum; p++) {
 			ans = (parseInt((p - 1) / 10) % 10) + (((p - 1) % 10) * 3);
 			patten = c.substring(ans + 10, ans + 13);
-			var tmpURL = host + "/" + (p < 10 ? "00" + p : p < 100 ? "0" + p : p) + "_" + patten + ".jpg";
+			var tmpURL = curHost + "/" + (p < 10 ? "00" + p : p < 100 ? "0" + p : p) + "_" + patten + ".jpg";
 			picAy[p-1] = tmpURL;
 		}
 
@@ -30,7 +33,7 @@ var calPictureURL = function (cs, ch, host, callback) {
 
 var processing = function (callback) {
 	$.get(firstPage, function(res) {
-		var preVolURL, nextVolURL, picCount, something, mapping = {}, thePic, startSymbol, host, ch;
+		var preVolURLL, picCount, something, mapping = {}, thePic, startSymbol;
 		thePic = $('#TheImg')[0].src;
 		startSymbol = thePic.match(/[0-9]*_([a-z0-9]*).jpg$/);
 		ch = location.search.match(/[0-9]+$/);
@@ -45,57 +48,63 @@ var processing = function (callback) {
 		
 		var picAy, patten, ans, c;
 
-		calPictureURL(mapping.cs, ch, host, function(pics) {
+		calPictureURL(mapping.cs, function(pics) {
 			picAy = pics;
 		});
 		
-		callback(ch, picAy, nextVolURL, host);
-	});
+		callback(picAy); 
+    });
 };
 
-processing(function(ch, pic, nextVolURL, host){
-	$('html').html('<!DOCTYPE HTML><head></head><title></title></head><body></body>');
 
-	for (var i = 0; i != pic.length; i++)
-		$('body').append("<div><img src='" + pic[i] + "'></div>");
-
-	window.addEventListener('scroll', function() { 
-		console.log(window.innerHeight + 1 + $(window).scrollTop());
-		$(window).scroll(function() {
-   			if(window.innerHeight + $(window).scrollTop() + 1 == $(document).height()) {
-    	   		 alert("bottom!");
-    	   		 loadNext(nextVolURL, host, ch, function(nextURL, nextCh, pics) {
-    	   		 	 nextVolURL = nextURL;
-    	   		 	 ch = nextCh;
-    	   		 	 for (var i = 0; i != pics.length; i++) 
-    	   		 	 	 $('body').append("<div><img src='" + pics[i] + "'></div>");
-    	   		 });
-   			}
-   		}); 
-	},
-	false);
-});
-
-var loadNext = function(nextVolURL, host, ch, callback) {
+var loadNext = function(callback) {
+    console.log(nextVolURL);
 	$.get(nextVolURL, function(data) {
-		var picAy, something, mapping = {}, target, pagenum, curHost;
-		ch++;
+        var picAy, something, mapping = {}, target, pagenum, curHost;
+        ch++;
 		curHost += ch + "/";
 		nextVolURL = nextVolURL.replace(nextVolURL.match(/=([0-9]+)/)[1], ch);
-		console.log(nextVolURL);
 		target = data.search("var chs");
-		data = data.substring(target, data.length);
+		ata = data.substring(target, data.length);
 		target = data.search("</script>");
 		data = data.substring(0, target).replace(/var/g, "").replace(/[ \']/g, "").replace(/eval.*/, "");
 		something = data.split(";");
-		
+	    
 		for (var index in something) 
 			mapping[something[index].split("=")[0]] = something[index].split("=")[1];
-		
-		calPictureURL(mapping.cs, ch, host, function(pics) {
-			callback(nextVolURL, ch, pics);
+	    console.log(mapping.cs);	
+		calPictureURL(mapping.cs, function(pics) {
+			callback(pics);
 		});
 
 
 	}); 
 }
+
+$(document).ready(function() {
+    processing(function(pic) {
+        $('html').html('<head><title></title></head><body></body>');
+        for (var i = 0; i != pic.length; i++)
+            $('body').append("<div><img src='" + pic[i] + "'></img></div>");
+
+        window.addEventListener('scroll', function() { 
+            console.log("Window height: " + window.innerHeight + ", ScrollBar height: " + $(window).scrollTop() 
+                            + ", Document height: " + $(document).height());
+            console.log(loading);
+            $(window).scroll(function() {
+                if(window.innerHeight + $(window).scrollTop() + 50 >= $(document).height() && !loading) {
+                    loading = true;
+                    console.log("NewURL:" + nextVolURL);
+                    loadNext(function(pics) {
+                        nextVolURL = nextURL;
+                        ch = nextCh;
+                        for (var i = 0; i != pics.length; i++)
+                            $('body').append("<div><img src='" + pics[i] + "'></img></div>");
+                        loading = false;      
+                    });
+                }
+            }); 
+        }, false);
+    });
+});
+    
