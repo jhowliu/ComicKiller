ffbc6a27cfdbf4a4eb04569ebf0ec279acc48297
jhow <jhowliu@gmail.com>
2014-06-02 19:28:15 +0800

    Add dynamic loading comic list.

--- js/popup.js
+++ js/popup.js
@@ -4,7 +4,14 @@ loadComicList = function(data) {
     for (i = 0; i != data.length; i++) { 
         info = '<div class="info"><div class="currentVol' + i + '"><h4>Current Episode : "'  + data[i].currentVol + '"</h4></div><div class="lastVol' + i + '"><h4>Newest Episod : "' + data[i].lastVol + '"</h4></div></div>'
         // Set cover image.
-        $('#eightComic').append('<div><h3>' + data[i].title + '</h3><li><img src="' + data[i].coverImgURL + '"></img>' + info + '</li></div>');
+        switch(data[i].site) {
+            case '8comic':
+                $('#eightComic').append('<div><h3>' + data[i].title + '</h3><li><img src="' + data[i].coverImgURL + '"></img>' + info + '</li></div>');
+                break;
+            case 'dm5':
+                $('#dm5').append('<div><h3>' + data[i].title + '</h3><li><img src="' + data[i].coverImgURL + '"></img>' + info + '</li></div>');
+                break;
+        }
     }
 };
 
